47a189999462a01c9511d6e6e6ab84585a15db7b
Prashant Kumar Nayak <prashantnayak@Prashants-MacBook-Pro.local>
2018-06-30 20:24:51 +0530

    Initial commit

---
+++ AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Interview
+//
+//  Created by Prashant Kumar Nayak on 22/06/18.
+//  Copyright © 2018 Prashant Kumar Nayak. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  var appCoordinator: AppCoordinator!
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow.init(frame: UIScreen.main.bounds)
+    appCoordinator = AppCoordinator.init(window: window)
+    appCoordinator.start()
+    return true
+  }
+
+}
+
